141e11efba4828a00a07fc5243daf0c463a318fd
Elliot Blanchard <public@elliotblanchard.com>
2020-04-13 14:31:24 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min_value = 0
+  birthday_kids.collect do |kids_name, age|
+    if min_value != 0 && age < min_value
+      min_value = age
+    elsif min_value == 0
+      min_value = age
+    end  
+  end
+  min_value
 end
