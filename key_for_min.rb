9b73dc7c1394292b5c3a96a0575ce3509b350a7d
Elliot Blanchard <public@elliotblanchard.com>
2020-04-13 14:40:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,15 +4,18 @@
 def key_for_min_value(name_hash)
   min_value = 0
   min_key = ""
-  name_hash.collect do |key, value|
-    if min_value != 0 && value < min_value
-      min_value = value
-      min_key = key
-    elsif min_value == 0
-      min_value = value
-      min_key = key
+  if name_hash.empty?
+    nil
+  else
+    name_hash.collect do |key, value|
+      if min_value != 0 && value < min_value
+        min_value = value
+        min_key = key
+      elsif min_value == 0
+        min_value = value
+        min_key = key
+      end
     end
+    min_key
   end
-  min_value
-  min_key
 end
